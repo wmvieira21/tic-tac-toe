3b9be096f9f7531d07aab351e18bcfca753d0a63
wmvieira21 <williammartinsvieira334@gmail.com>
2025-07-22 13:14:40 -0300

    Refactor GameBoard component and update styles for better layout and functionality

--- src/components/board/GameBoard.jsx
+++ src/components/board/GameBoard.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
+
 const initialGameState = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
 ];
 
 function GameBoard() {
+    const [gameState, setGameState] = useState(initialGameState);
+
+    function handleCellClick(rowIndex, cellIndex) {
+        setGameState(prevState => {
+            const newState = [...prevState.map(row => [...row])];
+            newState[rowIndex][cellIndex] = 'X';
+            return newState;
+        });
+    }
+
     return (
         <ol id='game-board'>
-            {initialGameState.map((row, rowIndex) => (
+            {gameState.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((cell, cellIndex) => (
                             <li key={cellIndex}>
-                                <button>{cell}</button>
+                                <button onClick={() => handleCellClick(rowIndex, cellIndex)}>{cell}</button>
                             </li>
                         ))}
                     </ol>
